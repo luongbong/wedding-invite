8b1b880cdf9a30e0eb46680148ffd7ac92e3a707
luongbong <75796399+luongbong@users.noreply.github.com>
2026-01-31 09:34:37 +0000

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,21 @@
+const weddingDate = new Date("March 20, 2026 00:00:00").getTime();
+
+const countdownFunction = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = weddingDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((distance / 1000 / 60) % 60);
+  const seconds = Math.floor((distance / 1000) % 60);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (distance < 0) {
+    clearInterval(countdownFunction);
+    document.querySelector(".countdown").innerHTML = "💖 Hôm nay là ngày cưới!";
+  }
+}, 1000);
